c0c840b72be505a1afd30f1b0c037a972d76491b
Boxy <nozakisola@gmail.com>
2025-11-08 15:32:54 -0600

    static vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,11 @@ import preact from '@preact/preset-vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [preact()],
+	base: "/lazybones/"
+,	plugins: [preact()],
 	build: {
-		"outDir": "dist"
+		"outDir": "dist",
+		"emitAssets": true,
 	},
 	resolve: {
 		alias: {
